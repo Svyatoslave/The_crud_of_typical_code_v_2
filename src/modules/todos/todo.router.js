50ffbdeb6eaf3829e7004ef8ed25724a7ec263b7
Svyatoslav <svyatoslave07@gmail.com>
2021-08-05 20:26:36 +0300

    3.05

--- src/modules/todos/todo.router.js
+++ src/modules/todos/todo.router.js
@@ -2,10 +2,10 @@ import Router from "express";
 import todosController from "./todos.controller.js";
 
 const todosRouter = new Router();
-todosRouter.post(` todos`, todosController.create);
-todosRouter.get(` todos`, todosController.getAll);
-todosRouter.get(` todos/:id`, todosController.getOne);
-todosRouter.put(` todos/:id`, todosController.update);
-todosRouter.delete(` todos/:id`, todosController.delete);
+todosRouter.post(`/todos`, todosController.create);
+todosRouter.get(`/todos`, todosController.getAll);
+todosRouter.get(`/todos/:id`, todosController.getOne);
+todosRouter.put(`/todos/:id`, todosController.update);
+todosRouter.delete(`/todos/:id`, todosController.delete);
 
 export default todosRouter;
